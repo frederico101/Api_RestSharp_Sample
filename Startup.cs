2f571bd1bda9ed6a31019a431a71fe8929d662c3
frederico101 <fredyalves.fredyalves@hotmail.com>
2020-01-23 11:08:50 -0300

    first com

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
-﻿using ApiPNB.Interfaces;
+﻿using ApiPNB.InfraInstrutura;
+using ApiPNB.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
